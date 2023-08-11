446e877fb4ec37ff838c262ba78a59b301b51b4f
Felipe Gomes Pellegrini <107892258+FelipeGPellegrini@users.noreply.github.com>
2023-08-11 15:18:39 -0300

    Initial commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import styles from "./Footer.module.css"
+
+const Footer = () => {
+    return (
+        <footer className={styles.footer}>
+            <p>
+                <span>
+                    React + TS to-do
+                </span>
+                @ 2023
+            </p>
+        </footer>
+    )
+}
+
+export default Footer
